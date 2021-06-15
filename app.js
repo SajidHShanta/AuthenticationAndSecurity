08633f64916998e248dd432472923169691eda97
sajidHshanta <sajid.shanta@northsouth.edu>
2021-06-15 21:13:21 +0600

    Implement User LogOut

--- app.js
+++ app.js
@@ -73,6 +73,11 @@ app.get("/secrets", function(req, res){
   }
 });
 
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
 app.post("/register", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
